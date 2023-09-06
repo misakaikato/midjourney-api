276f4761d7fb6aa37db299ab2b30455363387a18
misakaikato <misakaikato@outlook.com>
2023-09-06 16:37:12 +0800

    🚀 feat: 新指令的测试

--- src/server/routers/task/task.test.client.ts
+++ src/server/routers/task/task.test.client.ts
@@ -16,34 +16,53 @@ socket.on("TaskEvent", (task: any) => {
 const INTERVAL = 3000
 
 socket.on("connect", async () => {
-    // for (let i = 0; i < 100; i++) {
+    // socket.emit("SubmitTask", {
+    //     id: `imageine`,
+    //     userId: process.env.USERID ?? "misaka",
+    //     type: "imagine",
+    //     data: {
+    //         prompt: "a cute cat --niji",
+    //         images: [ "https://fanyiapp.cdn.bcebos.com/feed/d13329d0-6e15-3a56-f908-43e6e9ab2fc8.jpg" ]
+    //     }
+    // }, "imagine");
     socket.emit("SubmitTask", {
-        id: `shorten-test`,
+        id: `reroll`,
         userId: process.env.USERID ?? "misaka",
-        type: "Imagine",
-        data: { prompt: "a biger apple???" }
-    }, "shorten");
-    await sleep(INTERVAL);
-    socket.emit("SubmitTask", {
-        id: `0001`,
-        userId: process.env.USERID ?? "misaka",
-        type: "Imagine",
-        data: { prompt: "a cute cat" }
-    }, "imagine");
-    await sleep(INTERVAL);
-    socket.emit("SubmitTask", {
-        id: `0002`,
-        userId: process.env.USERID ?? "misaka",
-        type: "Imagine",
-        data: { prompt: "a cute cat" }
-    }, "imagine");
-    await sleep(INTERVAL);
-    socket.emit("SubmitTask", {
-        id: `0003`,
-        userId: process.env.USERID ?? "misaka",
-        type: "Imagine",
-        data: { prompt: "a cute cat" }
-    }, "imagine");
+        type: "reroll",
+        data: {
+            taskMsg: {
+                "id": "1148519326001926234",
+                "flags": 0,
+                "content": "**<<<749632982335>>> <https://s.mj.run/P7ki_AQe53g> a cute cat --niji --s 750** - <@914174839277899777> (fast)",
+                "hash": "e35ca991-f717-49e6-9fdb-eb3681e5532a",
+            },
+            direction: "left"
+        }
+    }, "reroll");
+    // socket.emit("SubmitTask", {
+    //     id: `shorten-test`,
+    //     userId: process.env.USERID ?? "misaka",
+    //     type: "Imagine",
+    //     data: { prompt: "a biger apple???" }
+    // }, "shorten");
+    // socket.emit("SubmitTask", {
+    //     id: `0001`,
+    //     userId: process.env.USERID ?? "misaka",
+    //     type: "Imagine",
+    //     data: { prompt: "a cute cat" }
+    // }, "imagine");
+    // socket.emit("SubmitTask", {
+    //     id: `0002`,
+    //     userId: process.env.USERID ?? "misaka",
+    //     type: "Imagine",
+    //     data: { prompt: "a cute cat" }
+    // }, "imagine");
+    // socket.emit("SubmitTask", {
+    //     id: `0003`,
+    //     userId: process.env.USERID ?? "misaka",
+    //     type: "Imagine",
+    //     data: { prompt: "a cute cat" }
+    // }, "imagine");
 });
 
 // wsServer.on("connection", (socket) => {
